c3a53f949d9f8df536ad295108bfb73c998b53b1
Ornán Castillo <ornancastillo@gmail.com>
2025-06-15 13:52:16 -0700

    feature: dashboard

--- src/presentation/screens/dashboard/Home.jsx
+++ src/presentation/screens/dashboard/Home.jsx
@@ -4,6 +4,7 @@ import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Cell, PieChart, Pie, 
 import Loading from "../../components/shared/Loading";
 import ErrorPage from "../../components/shared/ErrorPage";
 import { Warehouse, Banknote, HandCoins } from 'lucide-react';
+import formatPrice from "../../../functions/FormatPrice";
 
 const INVENTARIO_INFO = gql`
     query GetPendingInventory($tipo: Int) {
@@ -16,38 +17,30 @@ const INVENTARIO_INFO = gql`
     }
 `;
 
-const monthlyData = [
-    {
-        month: "Mes Anterior",
-        amount: 45750,
-        color: "#6b7280",
-        label: "Noviembre 2024",
-    },
-    {
-        month: "Mes Actual",
-        amount: 52300,
-        color: "#10b981",
-        label: "Diciembre 2024",
-    },
-]
+const SALES_INFO = gql`
+    query GetSalesAmount($tipo: Int) {
+        getSalesAmount(tipo: $tipo) {
+            month
+            amount
+            color
+        }
+    }
+`;
 
-const formatCurrency = (value) => {
-    return new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 0,
-    }).format(value)
-}
-
+const PAYMENT_INFO = gql`
+    query GetPayments($tipo: Int) {
+        getPayments(tipo: $tipo) {
+            month
+            amount
+            color
+        }
+    }
+`;
 
 const CustomTooltip = ({ active, payload }) => {
-    console.log(active, payload);
     
     if (active && payload && payload.length) {
 
-         console.log(active, payload);
-    
-    
         return (
             <div className="bg-white p-4 border border-gray-200 rounded-lg shadow-lg">
                 <span>{payload[0].name}: {payload[0].value}</span>
@@ -79,12 +72,6 @@ const CustomLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) =>
 
 export default function Dashboard() {
   
-    const lastMonth = monthlyData[0].amount
-    const currentMonth = monthlyData[1].amount
-    const difference = currentMonth - lastMonth
-    const percentageChange = ((difference / lastMonth) * 100).toFixed(1)
-    const isPositive = difference > 0
-
     const [activeTab, setActiveTab] = useState(1);
     const [activeGraph, setActiveGraph] = useState(1);
 
@@ -97,7 +84,25 @@ export default function Dashboard() {
         }
     );
 
-    if(loading){
+    const { loading: loadingSales, data: dataSales, error: errorSales } = useQuery(SALES_INFO, 
+        { 
+            fetchPolicy:"network-only", 
+            variables:{
+                tipo: activeTab
+            }
+        }
+    );
+
+    const { loading: loadingPayment, data: dataPayment, error: errorPayment } = useQuery(PAYMENT_INFO, 
+        { 
+            fetchPolicy:"network-only", 
+            variables:{
+                tipo: activeTab
+            }
+        }
+    );
+
+    if(loading || loadingSales || loadingPayment){
         return (
             <div className="min-h-screen flex items-center justify-center flex-col">
                 <h1 className="text-3xl font-bold text-gray-800 mb-5">Cargando</h1>
@@ -106,13 +111,10 @@ export default function Dashboard() {
         );
     }
 
-    if(error) {
+    if(error || errorSales || errorPayment) {
         return <ErrorPage message={"Inténtelo más tarde."}/>
     }
 
-    console.log(data);
-    
- 
     return (
         <div className="w-full lg:pl-64 md:pl-64 h-screen flex justify-center">
             
@@ -170,7 +172,7 @@ export default function Dashboard() {
                         </div>
                     </div>
                 </div>
-                {activeGraph ===  1 || activeGraph ===  3 ? 
+                {activeGraph ===  1  ? 
                     <div className="bg-white rounded-xl shadow-lg p-8 mb-8">
                         <div className="text-center mb-6">
                             <h2 className="text-2xl font-bold text-gray-800 mb-2">Estado del Inventario</h2>
@@ -251,14 +253,14 @@ export default function Dashboard() {
                 {activeGraph ===  2 ? 
                     <div className="bg-white rounded-xl shadow-lg p-8">
                         <div className="text-center mb-6">
-                            <h2 className="text-2xl font-bold text-gray-800 mb-2">Comparación de Cobranza</h2>
+                            <h2 className="text-2xl font-bold text-gray-800 mb-2">Comparación de Ventas</h2>
                             <p className="text-gray-600">Total de ingresos entre meses</p>
                         </div>
                         <div className="grid lg:grid-cols-2 gap-8">
                             <div className="h-64">
                                 <ResponsiveContainer width="100%" height="100%">
                                     <BarChart
-                                        data={monthlyData}
+                                        data={dataSales.getSalesAmount}
                                         margin={{
                                             top: 20,
                                             right: 30,
@@ -277,10 +279,10 @@ export default function Dashboard() {
                                             tick={{ fontSize: 10, fill: "#6b7280" }}
                                             axisLine={false}
                                             tickLine={false}
-                                            tickFormatter={formatCurrency}
+                                            tickFormatter={formatPrice}
                                         />
                                         <Bar dataKey="amount" radius={[6, 6, 0, 0]}>
-                                            {monthlyData.map((entry, index) => (
+                                            {dataSales.getSalesAmount.map((entry, index) => (
                                                 <Cell key={`cell-${index}`} fill={entry.color} />
                                             ))}
                                         </Bar>
@@ -288,7 +290,7 @@ export default function Dashboard() {
                                 </ResponsiveContainer>
                             </div>
                             <div className="space-y-4">
-                                {monthlyData.map((month, index) => (
+                                {dataSales.getSalesAmount.map((month, index) => (
                                     <div
                                         key={month.month}
                                         className={`p-4 rounded-lg border ${
@@ -298,11 +300,10 @@ export default function Dashboard() {
                                         <div className="flex justify-between items-center">
                                             <div>
                                                 <h4 className="font-semibold text-gray-800">{month.month}</h4>
-                                                <p className="text-xs text-gray-500">{month.label}</p>
                                             </div>
                                             <div className="text-right">
                                                 <p className="text-xl font-bold" style={{ color: month.color }}>
-                                                {formatCurrency(month.amount)}
+                                                {formatPrice(month.amount)}
                                                 </p>
                                             </div>
                                         </div>
@@ -310,17 +311,101 @@ export default function Dashboard() {
                                 ))}
                                 <div
                                 className={`p-4 rounded-lg border-2 ${
-                                    isPositive ? "bg-green-50 border-green-200" : "bg-red-50 border-red-200"
+                                    dataSales.getSalesAmount[0].amount <= dataSales.getSalesAmount[1].amount ? "bg-green-50 border-green-200" : "bg-red-50 border-red-200"
                                 }`}
                                 >
-                                    <div className="text-center">
-                                        <h4 className="font-semibold text-gray-800 mb-2">Cambio Mensual</h4>
-                                        <p className={`text-lg font-bold ${isPositive ? "text-green-600" : "text-red-600"}`}>
-                                            {isPositive ? "+" : ""}
-                                            {formatCurrency(difference)} ({isPositive ? "+" : ""}
-                                            {percentageChange}%)
+                                    <div className="">
+                                        <h4 className="font-semibold text-gray-800 mb-2 text-center">Cambio Mensual</h4>
+                                        <p className={`text-lg font-bold ${dataSales.getSalesAmount[0].amount <= dataSales.getSalesAmount[1].amount ? "text-green-600" : "text-red-600"} text-center`}>
+                                            {dataSales.getSalesAmount[0].amount <= dataSales.getSalesAmount[1].amount ? "+" : ""}
+                                            {formatPrice(dataSales.getSalesAmount[1].amount - dataSales.getSalesAmount[0].amount)} 
                                         </p>
-                                        <p className="text-sm text-gray-600 mt-1">{isPositive ? "↗ Crecimiento" : "↘ Disminución"}</p>
+                                        <p className={`text-lg font-bold ${dataSales.getSalesAmount[0].amount <= dataSales.getSalesAmount[1].amount ? "text-green-600" : "text-red-600"} text-center`}>
+                                            ({dataSales.getSalesAmount[0].amount <= dataSales.getSalesAmount[1].amount ? "+" : ""}
+                                                {dataSales.getSalesAmount[0].amount > 0 ?  (((dataSales.getSalesAmount[0].amount - dataSales.getSalesAmount[1].amount) / dataSales.getSalesAmount[0].amount) * 100).toFixed(2) : 0}%)
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                : 
+                    null
+                }
+                {activeGraph ===  3 ? 
+                    <div className="bg-white rounded-xl shadow-lg p-8">
+                        <div className="text-center mb-6">
+                            <h2 className="text-2xl font-bold text-gray-800 mb-2">Comparación de Cobranza</h2>
+                            <p className="text-gray-600">Total de cobranza entre meses</p>
+                        </div>
+                        <div className="grid lg:grid-cols-2 gap-8">
+                            <div className="h-64">
+                                <ResponsiveContainer width="100%" height="100%">
+                                    <BarChart
+                                        data={dataPayment.getPayments}
+                                        margin={{
+                                            top: 20,
+                                            right: 30,
+                                            left: 20,
+                                            bottom: 5,
+                                        }}
+                                        barCategoryGap="40%"
+                                    >
+                                        <XAxis
+                                            dataKey="month"
+                                            tick={{ fontSize: 12, fill: "#374151", fontWeight: 500 }}
+                                            axisLine={false}
+                                            tickLine={false}
+                                        />
+                                        <YAxis
+                                            tick={{ fontSize: 10, fill: "#6b7280" }}
+                                            axisLine={false}
+                                            tickLine={false}
+                                            tickFormatter={formatPrice}
+                                        />
+                                        <Bar dataKey="amount" radius={[6, 6, 0, 0]}>
+                                            {dataPayment.getPayments.map((entry, index) => (
+                                                <Cell key={`cell-${index}`} fill={entry.color} />
+                                            ))}
+                                        </Bar>
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            </div>
+                            <div className="space-y-4">
+                                {dataPayment.getPayments.map((month, index) => (
+                                    <div
+                                        key={month.month}
+                                        className={`p-4 rounded-lg border ${
+                                        month.month === "Mes Actual" ? "border-green-200 bg-green-50" : "border-gray-200 bg-gray-50"
+                                        }`}
+                                    >
+                                        <div className="flex justify-between items-center">
+                                            <div>
+                                                <h4 className="font-semibold text-gray-800">{month.month}</h4>
+                                            </div>
+                                            <div className="text-right">
+                                                <p className="text-xl font-bold" style={{ color: month.color }}>
+                                                {formatPrice(month.amount)}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                                <div
+                                className={`p-4 rounded-lg border-2 ${
+                                    dataPayment.getPayments[0].amount <= dataPayment.getPayments[1].amount ? "bg-green-50 border-green-200" : "bg-red-50 border-red-200"
+                                }`}
+                                >
+                                    <div className="">
+                                        <h4 className="font-semibold text-gray-800 mb-2 text-center">Cambio Mensual</h4>
+                                        <p className={`text-lg font-bold ${dataPayment.getPayments[0].amount <= dataPayment.getPayments[1].amount ? "text-green-600" : "text-red-600"} text-center`}>
+                                            {dataPayment.getPayments[0].amount <= dataPayment.getPayments[1].amount ? "+" : ""}
+                                            {formatPrice(dataPayment.getPayments[1].amount - dataPayment.getPayments[0].amount)} 
+                                        </p>
+                                        <p className={`text-lg font-bold ${dataPayment.getPayments[0].amount <= dataPayment.getPayments[1].amount ? "text-green-600" : "text-red-600"} text-center`}>
+                                            ({dataPayment.getPayments[0].amount <= dataPayment.getPayments[1].amount ? "+" : ""}
+                                                {dataPayment.getPayments[0].amount > 0 ?  (((dataPayment.getPayments[0].amount - dataPayment.getPayments[1].amount) / dataPayment.getPayments[0].amount) * 100).toFixed(2) : 0}%)
+                                        </p>
                                     </div>
                                 </div>
                             </div>
